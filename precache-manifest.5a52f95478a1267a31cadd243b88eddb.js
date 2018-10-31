f706d2ff2fbc1027e98d885f5a9874b8f6208208
Clara Olson <cjolson@andrew.cmu.edu>
2018-10-30 21:56:01 -0400

    Updates

---
+++ precache-manifest.5a52f95478a1267a31cadd243b88eddb.js
@@ -0,0 +1,202 @@
+self.__precacheManifest = [
+  {
+    "revision": "40bcc44207879dae8fcdbc069385e5e8",
+    "url": "/santacruz/static/media/label_santacruz.40bcc442.png"
+  },
+  {
+    "revision": "1966debb41cdbb15462e",
+    "url": "/santacruz/static/css/main.4ecb705b.chunk.css"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/santacruz/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "d10afb60d40cf6d166e6",
+    "url": "/santacruz/static/js/runtime~main.d10afb60.js"
+  },
+  {
+    "revision": "5b3573eb6f4d06893324a25817cdd1b6",
+    "url": "/santacruz/static/media/aptos1.5b3573eb.jpg"
+  },
+  {
+    "revision": "5312492393a8cf91afea9459954fcf27",
+    "url": "/santacruz/static/media/aptos2.53124923.jpg"
+  },
+  {
+    "revision": "b10973e0cc78105a06015157310dddd6",
+    "url": "/santacruz/static/media/aptos3.b10973e0.jpg"
+  },
+  {
+    "revision": "3f705de5f0fc37ea481374fbd1f2b42c",
+    "url": "/santacruz/static/media/aptos4.3f705de5.jpg"
+  },
+  {
+    "revision": "d8a0358a91dd9ee957fbd1b7474d56b2",
+    "url": "/santacruz/static/media/aptos5.d8a0358a.jpg"
+  },
+  {
+    "revision": "1fc3132cdf8dd47631b06ce060bcf0bc",
+    "url": "/santacruz/static/media/aptos6.1fc3132c.jpg"
+  },
+  {
+    "revision": "65d51bc09bc532897bc8889c672c9393",
+    "url": "/santacruz/static/media/capitola1.65d51bc0.jpg"
+  },
+  {
+    "revision": "6eaadcddf15aea158b13b0441b51ffdf",
+    "url": "/santacruz/static/media/capitola2.6eaadcdd.jpg"
+  },
+  {
+    "revision": "4798a5b7477ca1b5b2ac151872b30243",
+    "url": "/santacruz/static/media/capitola3.4798a5b7.jpg"
+  },
+  {
+    "revision": "b5cf5e3778f1903e7b5158c020825ecb",
+    "url": "/santacruz/static/media/capitola4.b5cf5e37.jpg"
+  },
+  {
+    "revision": "e65decf7cba9adffc7a0a61c0ac2f99d",
+    "url": "/santacruz/static/media/capitola5.e65decf7.jpg"
+  },
+  {
+    "revision": "137acf24e4e96ce983e36b53f7edf7c6",
+    "url": "/santacruz/static/media/capitola6.137acf24.jpg"
+  },
+  {
+    "revision": "6f1bd5f80236fe1ae936f39bacf5ce9a",
+    "url": "/santacruz/static/media/davenport1.6f1bd5f8.jpg"
+  },
+  {
+    "revision": "8b0a85175dd61e7e030a40f23b5f0c01",
+    "url": "/santacruz/static/media/davenport2.8b0a8517.jpg"
+  },
+  {
+    "revision": "54ea9de4f8d5166061bb52bf674efc38",
+    "url": "/santacruz/static/media/davenport3.54ea9de4.jpg"
+  },
+  {
+    "revision": "576ef6df5a31a248bed1549169cb4edb",
+    "url": "/santacruz/static/media/davenport4.576ef6df.jpg"
+  },
+  {
+    "revision": "e190682c938e60166259bd231170eea1",
+    "url": "/santacruz/static/media/davenport5.e190682c.jpg"
+  },
+  {
+    "revision": "f802c4edb0762b89b4ba368d1133ddac",
+    "url": "/santacruz/static/media/davenport6.f802c4ed.jpg"
+  },
+  {
+    "revision": "255afbe5a305e97483c84737340e685e",
+    "url": "/santacruz/static/media/label_aptos.255afbe5.png"
+  },
+  {
+    "revision": "56d52ccda1a122ade396b2ecf4c9be83",
+    "url": "/santacruz/static/media/label_capitola.56d52ccd.png"
+  },
+  {
+    "revision": "7576a9c49bd2ca191a9494931f9f4a8c",
+    "url": "/santacruz/static/media/label_davenport.7576a9c4.png"
+  },
+  {
+    "revision": "1966debb41cdbb15462e",
+    "url": "/santacruz/static/js/main.1966debb.chunk.js"
+  },
+  {
+    "revision": "c92f00016def4821f198242410b62253",
+    "url": "/santacruz/static/media/label_scottsvalley.c92f0001.png"
+  },
+  {
+    "revision": "baeca01be270e799acc00133de76381c",
+    "url": "/santacruz/static/media/label_slv.baeca01b.png"
+  },
+  {
+    "revision": "f360a33fa351bdabf5e7f952c5ea2978",
+    "url": "/santacruz/static/media/pebbles.f360a33f.jpg"
+  },
+  {
+    "revision": "7617d8c144ed67a98af6d4d91808eb0f",
+    "url": "/santacruz/static/media/santacruz1.7617d8c1.jpg"
+  },
+  {
+    "revision": "85703b22d80f019a1277e6cfcda90c10",
+    "url": "/santacruz/static/media/santacruz2.85703b22.jpg"
+  },
+  {
+    "revision": "b53a18e9942b10bf19511ece30f608c3",
+    "url": "/santacruz/static/media/santacruz3.b53a18e9.jpg"
+  },
+  {
+    "revision": "119960c25b13b46e46b4038b0df2a0aa",
+    "url": "/santacruz/static/media/santacruz4.119960c2.jpg"
+  },
+  {
+    "revision": "6d7fd7731a4fb8d20f6b0d53346f0018",
+    "url": "/santacruz/static/media/santacruz5.6d7fd773.jpg"
+  },
+  {
+    "revision": "a884b5916798cb31cc6fcbf3da0407b8",
+    "url": "/santacruz/static/media/santacruz6.a884b591.jpg"
+  },
+  {
+    "revision": "115b02025f1aec8714e05e42ff153ef7",
+    "url": "/santacruz/static/media/scottsvalley1.115b0202.jpg"
+  },
+  {
+    "revision": "e6b5dffc0652faff112e4144fbde8528",
+    "url": "/santacruz/static/media/scottsvalley2.e6b5dffc.jpg"
+  },
+  {
+    "revision": "17febcfe18f5b3395794ca3e45e805c3",
+    "url": "/santacruz/static/media/scottsvalley3.17febcfe.jpg"
+  },
+  {
+    "revision": "4bef113fd6e8f66f5d45c4a53e35dc2c",
+    "url": "/santacruz/static/media/scottsvalley4.4bef113f.jpg"
+  },
+  {
+    "revision": "a9ed9bcab262c58c8204956ab93a4834",
+    "url": "/santacruz/static/media/scottsvalley5.a9ed9bca.jpg"
+  },
+  {
+    "revision": "14b69012ed10ccdce4c2e8a32c6d01f4",
+    "url": "/santacruz/static/media/scottsvalley6.14b69012.jpg"
+  },
+  {
+    "revision": "6865e84342f1550fdfcf424262581a7b",
+    "url": "/santacruz/static/media/slv1.6865e843.jpg"
+  },
+  {
+    "revision": "70257999e133b7968b0e784abe485e42",
+    "url": "/santacruz/static/media/slv2.70257999.jpg"
+  },
+  {
+    "revision": "6c6f0612ec74388d005b000891e6b131",
+    "url": "/santacruz/static/media/slv3.6c6f0612.jpg"
+  },
+  {
+    "revision": "3df78565f48718a9e44a65124c619a4e",
+    "url": "/santacruz/static/media/slv4.3df78565.jpg"
+  },
+  {
+    "revision": "d19fb4ea07c04c45795d669dcc7729e2",
+    "url": "/santacruz/static/media/slv5.d19fb4ea.jpg"
+  },
+  {
+    "revision": "1ddfec2065fd6ef418b041bf29e3c35f",
+    "url": "/santacruz/static/media/slv6.1ddfec20.jpg"
+  },
+  {
+    "revision": "a892b1fa0c80ce73d0062d992cfc8ee3",
+    "url": "/santacruz/static/media/logo.a892b1fa.png"
+  },
+  {
+    "revision": "2b76bf196dfb555adc0e7e2b0b2e9c83",
+    "url": "/santacruz/static/media/map.2b76bf19.png"
+  },
+  {
+    "revision": "32b79ae90636dbb36bcb1957f48fb93e",
+    "url": "/santacruz/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.2/workbox-sw.js");
+
+importScripts(
+  "/santacruz/precache-manifest.5a52f95478a1267a31cadd243b88eddb.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/santacruz/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
